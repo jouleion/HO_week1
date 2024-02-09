bef023c8d42a30ce77f5d62f24ee63e6674c0358
jouleion <124351760+jouleion@users.noreply.github.com>
2024-02-09 12:54:39 +0100

    added color cirlces into the visualizer

--- Assignment/main.py
+++ Assignment/main.py
@@ -54,7 +54,53 @@ pygame.display.set_caption("Haptic Skin visualiser")
 # Initialise the PyGame Clock for timing
 clock = pygame.time.Clock()
 
+def check_color_direction(color, dir):
+    new_dir = dir
+    if dir > 0:
+        if color > 230:
+            new_dir = -1
+    else:
+        if color < 50:
+            new_dir = 1
+    return new_dir
+
+
+def update_color():
+    global r, g, b
+    global rdir, gdir, bdir
+
+    # update color values, move color direction randomly, control direction of movement
+    r += int(rfac * rdir * random.random() * color_speed)
+    g += int(gfac * gdir * random.random() * color_speed)
+    b += int(bfac * bdir * random.random() * color_speed)
+
+    # limit color direction
+    rdir = check_color_direction(r, rdir)
+    gdir = check_color_direction(g, gdir)
+    bdir = check_color_direction(b, bdir)
+
+    color = pygame.Color(r, g, b)
+    return color
+
+
+def draw_circle():
+    # update color
+    circle_color = update_color()
+
+    # generate new random position for next circle
+    random_position = [random.randint(0, screen.get_width()), random.randint(0, screen.get_height())]
+
+    # generate random radius
+    random_radius = random.randint(5, 20)
+
+    # draw each cirlce
+    # pygame.draw.circle(screen, circle_color, random_position, random_radius)
+    gfxdraw.aacircle(screen, random_position[0], random_position[1], random_radius, circle_color)
+    gfxdraw.filled_circle(screen, random_position[0], random_position[1], random_radius, circle_color)
+
+
 running = True
+
 while TSP.available and running:
     # Check if the screen is closed and quit
     for event in pygame.event.get():

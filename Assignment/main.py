1baa456e0aca710621359ca954d794b6f6e873e2
jouleion <124351760+jouleion@users.noreply.github.com>
2024-02-09 15:44:13 +0100

    finished main to work with new particle system

--- Assignment/main.py
+++ Assignment/main.py
@@ -35,25 +35,35 @@ running = True
 particle_system = ParticleSystem()
 
 def update():
+    # Get the frame
+    grid = TSP.readFrame()
+
     # Clear the screen by blacking it out
-    gfxdraw.box(screen, (0, 0, 700, 700), (0, 0, 0, 10))
+    gfxdraw.box(screen, (0, 0, WINDOW_SIZE[0], WINDOW_SIZE[1]), (0, 0, 0, 10))
 
-    # simulate particles being added where screen is being pressed
-    for x in range(20):
-        for y in range(20):
-            if (random.randint(0, 100) > 98):
-                particle_system.add_particle(x * 30, y * 30)
+    # Loop through all pixels in the frame
+    for row in range(rows):
+        for column in range(columns):
+            # Get the pixel value and set the gray value accordingly
+            pixel = grid[row][column]
 
-    # update and draw particles
+            # spawn particles
+            if (pixel > 40):
+                for i in range(random.randint(1, int(pixel / 40))):
+                    particle_system.add_particle(50, 50)
+    # update particles
     particle_system.update()
 
 
 def draw():
+    # Clear the screen by blacking it out
+    gfxdraw.box(screen, (0, 0, 700, 700), (0, 0, 0, 10))
+
+    # draw particles
     particle_system.draw(screen)
 
     # Draw to the display
     pygame.display.flip()
-
 
 
 while TSP.available and running:
@@ -63,46 +73,3 @@ while TSP.available and running:
             pygame.quit()
             running = False
             exit()
-
-    # Get the frame
-    grid = TSP.readFrame()
-
-    # Clear the screen by blacking it out
-    gfxdraw.box(screen,  (0,0,WINDOW_SIZE[0],WINDOW_SIZE[1]),(0,0,0,10)  )
-
-    # Loop through all pixels in the frame
-    for row in range(rows):
-        for column in range(columns):
-            # Get the pixel value and set the gray value accordingly
-            pixel = grid[row][column]
-
-            # color = (pixel, pixel, pixel)
-
-            # draw circles
-            if(pixel > 40):
-                for i in range(random.randint(1,int(pixel/40))):
-                    draw_circle(row, column, pixel)
-
-            # Draw the pixel on the screen
-            """pygame.draw.rect(
-                screen,
-                color,
-                [
-                    PIXEL_MARGIN + ((PIXEL_MARGIN + PIXEL_WIDTH) * column),
-                    PIXEL_MARGIN + ((PIXEL_MARGIN + PIXEL_HEIGHT) * row),
-                    PIXEL_WIDTH,
-                    PIXEL_HEIGHT
-                ]
-            )"""
-
-    particle_system.add_particle(50, 50)
-    particle_system.update()
-    particle_system.draw(screen)
-
-    # Limit the framerate to 60FPS
-    # consistent FPS
-    dt = clock.tick(60) / 1000
-
-    # Draw to the display
-    pygame.display.flip()
-
